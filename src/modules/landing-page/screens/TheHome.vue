b0c2ff81367b1525f9b9e5808fe628a5114af1e4
Roger Santos <roger.santos36@gmail.com>
2024-11-02 19:01:02 -0300

    feat: add first part of hero section

---
+++ src/modules/landing-page/screens/TheHome.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import TheHeader from "@/modules/landing-page/components/TheHeader/TheHeader.vue";
+</script>
+
+<template>
+  <div class="container flex h-full w-full flex-col px-4">
+    <TheHeader class="mb-48 mt-8" />
+
+    <div class="h-max w-full flex-1">
+      <div class="mx-auto h-full w-full max-w-screen-xl p-5">
+        <h1 class="mb-6 text-center text-6xl font-bold text-brand-neutral-50">
+          Tsks, just tasks<span class="text-brand-gradient">.</span>
+        </h1>
+
+        <p class="mx-auto mb-12 max-w-sm text-center text-xl text-brand-neutral-300">
+          Keep track of the daily tasks in life and get that satisfaction upon completion.
+        </p>
+
+        <div class="flex items-center justify-center gap-3">
+          <button
+            type="button"
+            class="brand-gradient rounded-xl px-8 py-4 font-semibold text-brand-neutral-50 transition hover:opacity-90"
+          >
+            Get Started!
+          </button>
+          <button
+            type="button"
+            class="rounded-xl bg-brand-neutral-500 px-8 py-4 font-semibold text-brand-neutral-50 transition hover:bg-brand-neutral-600"
+          >
+            Learn More
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import BaseLayout from "@/layouts/BaseLayout.vue";
+import TheHome from "@/modules/landing-page/screens/TheHome.vue";
 </script>
 
 <template>
-  <BaseLayout />
+  <BaseLayout>
+    <TheHome />
+  </BaseLayout>
 </template>
