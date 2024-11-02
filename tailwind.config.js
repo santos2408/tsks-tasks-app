52422492e3b10b917ed78a0de9ffce046c68daef
Roger Santos <roger.santos36@gmail.com>
2024-11-02 17:49:49 -0300

    chore: add cdd and tailwind variables for style guide project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,48 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-primary": {
+          50: "rgb(var(--brand-primary-50))",
+          100: "rgb(var(--brand-primary-100))",
+          200: "rgb(var(--brand-primary-200))",
+          300: "rgb(var(--brand-primary-300))",
+          400: "rgb(var(--brand-primary-400))",
+          500: "rgb(var(--brand-primary-500))",
+          600: "rgb(var(--brand-primary-600))",
+          700: "rgb(var(--brand-primary-700))",
+          800: "rgb(var(--brand-primary-800))",
+          900: "rgb(var(--brand-primary-900))",
+        },
+
+        "brand-secondary": {
+          50: "rgb(var(--brand-secondary-50))",
+          100: "rgb(var(--brand-secondary-100))",
+          200: "rgb(var(--brand-secondary-200))",
+          300: "rgb(var(--brand-secondary-300))",
+          400: "rgb(var(--brand-secondary-400))",
+          500: "rgb(var(--brand-secondary-500))",
+          600: "rgb(var(--brand-secondary-600))",
+          700: "rgb(var(--brand-secondary-700))",
+          800: "rgb(var(--brand-secondary-800))",
+          900: "rgb(var(--brand-secondary-900))",
+        },
+
+        "brand-neutral": {
+          50: "rgb(var(--brand-neutral-50))",
+          100: "rgb(var(--brand-neutral-100))",
+          200: "rgb(var(--brand-neutral-200))",
+          300: "rgb(var(--brand-neutral-300))",
+          400: "rgb(var(--brand-neutral-400))",
+          500: "rgb(var(--brand-neutral-500))",
+          600: "rgb(var(--brand-neutral-600))",
+          700: "rgb(var(--brand-neutral-700))",
+          800: "rgb(var(--brand-neutral-800))",
+          900: "rgb(var(--brand-neutral-900))",
+        },
+      },
+    },
   },
   plugins: [],
 };
