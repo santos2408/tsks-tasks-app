579a203ee687a06b233616ca72b881fa23264fe3
Roger Santos <roger.santos36@gmail.com>
2024-11-02 18:59:53 -0300

    feat: add TheHeader component

---
+++ src/modules/landing-page/components/TheHeader/TheHeader.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import LogoComponent from "@/components/LogoComponent/LogoComponent.vue";
+</script>
+
+<template>
+  <header class="flex-1">
+    <nav class="flex items-center justify-between">
+      <div>
+        <RouterLink to="/">
+          <LogoComponent />
+        </RouterLink>
+      </div>
+
+      <div class="flex items-center gap-6">
+        <button
+          type="button"
+          class="text-brand-neutral-100 transition hover:text-brand-neutral-50"
+        >
+          Log in
+        </button>
+
+        <button
+          type="button"
+          class="rounded-xl border-2 border-brand-neutral-400 px-8 py-3 text-brand-neutral-100 transition hover:text-brand-neutral-50"
+        >
+          Sign up
+        </button>
+      </div>
+    </nav>
+  </header>
+</template>
