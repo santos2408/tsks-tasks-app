73d1a5174153d038a9762c38ccc89563fe4444c4
Roger Santos <roger.santos36@gmail.com>
2024-11-03 20:09:59 -0300

    feat: add dashboard image on home page

--- src/modules/landing-page/screens/TheHome.vue
+++ src/modules/landing-page/screens/TheHome.vue
@@ -3,34 +3,42 @@ import TheHeader from "@/modules/landing-page/components/TheHeader/TheHeader.vue
 </script>
 
 <template>
-  <div class="container flex h-full w-full flex-col px-4">
+  <div class="container relative flex h-full w-full flex-col px-4">
     <TheHeader class="mb-48 mt-8" />
 
-    <div class="h-max w-full flex-1">
-      <div class="mx-auto h-full w-full max-w-screen-xl p-5">
-        <h1 class="mb-6 text-center text-6xl font-bold text-brand-neutral-50">
-          Tsks, just tasks<span class="text-brand-gradient">.</span>
-        </h1>
+    <div class="mb-32">
+      <h1 class="mb-6 text-center text-6xl font-bold text-brand-neutral-50">
+        Tsks, just tasks<span class="text-brand-gradient">.</span>
+      </h1>
 
-        <p class="mx-auto mb-12 max-w-sm text-center text-xl text-brand-neutral-300">
-          Keep track of the daily tasks in life and get that satisfaction upon completion.
-        </p>
+      <p class="mx-auto mb-12 max-w-sm text-center text-xl text-brand-neutral-300">
+        Keep track of the daily tasks in life and get that satisfaction upon completion.
+      </p>
 
-        <div class="flex items-center justify-center gap-3">
-          <button
-            type="button"
-            class="brand-gradient rounded-xl px-8 py-4 font-semibold text-brand-neutral-50 transition hover:opacity-90"
-          >
-            Get Started!
-          </button>
-          <button
-            type="button"
-            class="rounded-xl bg-brand-neutral-500 px-8 py-4 font-semibold text-brand-neutral-50 transition hover:bg-brand-neutral-600"
-          >
-            Learn More
-          </button>
-        </div>
+      <div class="flex items-center justify-center gap-3">
+        <button
+          type="button"
+          class="brand-gradient rounded-xl px-8 py-4 font-semibold text-brand-neutral-50 transition hover:opacity-90"
+        >
+          Get Started!
+        </button>
+        <button
+          type="button"
+          class="rounded-xl bg-brand-neutral-500 px-8 py-4 font-semibold text-brand-neutral-50 transition hover:bg-brand-neutral-600"
+        >
+          Learn More
+        </button>
       </div>
+    </div>
+
+    <div class="relative">
+      <span
+        class="brand-gradient absolute -left-40 -top-48 h-[160px] w-[160px] rounded-full"
+      ></span>
+      <span
+        class="brand-gradient absolute -right-24 top-48 z-[-1] h-[180px] w-[180px] rounded-full"
+      ></span>
+      <img src="../../../assets/img/dashboard-home.png" alt="" />
     </div>
   </div>
 </template>
